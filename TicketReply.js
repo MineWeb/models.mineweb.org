2df2340d81d3e5ae18ddde04276002f7a73328db
vmarchaud <thisismac47@gmail.com>
2016-10-23 20:12:08 +0200

    Update TicketReply.js

--- TicketReply.js
+++ TicketReply.js
@@ -49,9 +49,15 @@ module.exports = {
     // hello
     var d = new Date()
     var moment = (d.getHours() > 3 && d.getHours() < 18) ? 'day' : 'night'
-    newContent += sails.config.ticket[lang].hello[moment] + "<br><br>" + content
+    if (sails.config.ticket[lang] !== undefined)
+      newContent += sails.config.ticket[lang].hello[moment] + "<br><br>" + content
+    else
+      newContent += sails.config.ticket['en-us'].hello[moment] + "<br><br>" + content
     // signature
-    var signature = sails.config.ticket[lang].signature
+    if (sails.config.ticket[lang] !== undefined)
+      var signature = sails.config.ticket[lang].signature
+    else
+      var signature = sails.config.ticket['en-us'].signature
     if (user.rolename === undefined)
       user.rolename = ''
     signature = signature.replace('{USERNAME}', user.username).replace('{ROLENAME}', user.rolename)
